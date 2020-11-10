567477b334915a15dd80b8da2d9a1cfdad5e9f3d
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-11-09 21:27:51 -0500

    Init Test Scene
    - Test MoonGravity and MouseLook scripts

---
+++ Moonshot/Assets/Scripts/Tests/MoonGravity.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonGravity : MonoBehaviour
+{
+    // Based on: http://answers.unity.com/answers/1785305/view.html
+
+    // Initialize
+    private Rigidbody rigidBody;
+    private Vector3 normal;
+    private Vector3 targetDirection;
+    private Quaternion targetRotation;
+
+    private const float GRAVITY = -20.0f;
+    private const float RAYDISTANCE = 1.0f;
+    private const float ROTATIONSPEED = 0.15f;
+
+    // When script initializes
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Physics update
+    void FixedUpdate()
+    {
+        // Set up ray to check the surface below player
+        RaycastHit temp_hit;
+        Ray temp_ray = new Ray(transform.position, -transform.up * RAYDISTANCE);
+
+        // Gets the normal of the surface below the character, and also creates a target direction for gravity
+        if (Physics.Raycast(temp_ray, out temp_hit) && temp_hit.transform.CompareTag("Moon"))
+        {
+            normal = temp_hit.normal;
+            targetDirection = (transform.position - temp_hit.point).normalized;
+        }
+
+        // Finds desired rotation relative to surface normal
+        targetRotation = Quaternion.FromToRotation(transform.up, normal) * transform.rotation;
+
+        // Apply rotation and gravity
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, ROTATIONSPEED);
+        
+        // Having a rigid body and 
+        if(rigidBody)
+            rigidBody.AddForce(targetDirection * GRAVITY);
+    }
+
+    private void Update()
+    {
+
+        if (transform.name.Equals("Player"))
+        {
+            // Input check for testing
+            if(Input.GetAxisRaw("Vertical") > 0)
+            {
+                rigidBody.AddForce(0.1f * transform.forward.normalized, ForceMode.Impulse);
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                rigidBody.AddForce(0.1f * -transform.forward.normalized, ForceMode.Impulse);
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                rigidBody.AddForce(0.1f * -transform.right, ForceMode.Impulse);
+            }
+
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                rigidBody.AddForce(0.1f * transform.right, ForceMode.Impulse);
+            }
+
+            if (Input.GetKeyDown("space"))
+            {
+                rigidBody.AddForce(20f * transform.up.normalized, ForceMode.Impulse);
+            }
+        }
+    }
+}

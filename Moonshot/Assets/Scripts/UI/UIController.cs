913da12c0204b71da3fa40627ff3f254d928a7cb
VincentBerardi <vincent.l.berardi@gmail.com>
2020-12-01 10:34:00 -0500

    Added Star tag, collider to StarBit prefab, UI for collecting stars, and UI logic

---
+++ Moonshot/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text textStarCounter;
+    public int starCounter;
+    public int levelMaxStars;
+    public PlayerController player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelStarCount();
+    }
+
+
+    private void LevelStarCount()
+    {
+        if (player.GotStar)
+        {
+            starCounter += 1;
+            player.GotStar = false;
+        }
+
+        textStarCounter.text = starCounter.ToString() + "/" + levelMaxStars.ToString();
+    }
+}
